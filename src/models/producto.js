b6d1600ef13768e91465b9f8d2e0a2cfd20103d8
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2022-03-10 13:51:42 -0300

    se agrego la logica para crear productos

--- src/models/producto.js
+++ src/models/producto.js
@@ -1,1 +1,28 @@
 // modelamos el dato a guardar en la base de datos
+import mongoose, {Schema} from "mongoose";
+
+const productoSchema = new Schema ({
+    productName:{
+        type: String,
+        maxlength: 200,
+        unique: true,
+        required: true
+    },
+    price:{
+        type: Number, 
+        required:true
+    },
+    urlImg:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    }
+});
+
+const Producto = mongoose.model("producto", productoSchema);
+
+export default Producto; 
+
